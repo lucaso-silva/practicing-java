0abc345415016b54dfe347d32d86019a3628dde5
lucaso-silva <luc.osilva@gmail.com>
2023-09-16 22:23:39 -0700

    working with inputted values - Scanner method

--- AccountTest.java
+++ AccountTest.java
@@ -1,25 +1,41 @@
+import java.util.Scanner;
+
 public class AccountTest
 {
 	public static void main(String[] args)
 	{
-		String name = "Lucas Oliveira",
-			   loginName = "loliveira10",
-			   instructorName = "Hengameh Hamavand";
-		int studentId = 10012345;
+		String name, loginName, instructorName;
+		int studentId;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter your name: ");
+		name = input.next();
+		
+		System.out.println("Please enter your network login: ");
+		loginName = input.next();
+		
+		System.out.println("Please enter your student ID: ");
+		studentId = input.nextInt();
+		
+		System.out.println("Please enter your instructor name: ");
+		instructorName = input.next();
 		
 		showCourseInfo( instructorName );
 		
 		System.out.println("My name is " + name + ".");
 		
-		System.out.println("My student ID is " +studentId);
-		System.out.println("My network login name is " + studentId + ".\n");
+		System.out.println("My student ID is " + studentId + ".");
+		System.out.println("My network login name is " + loginName + ".\n");
 		
 		System.out.println("\t****End of the Sample Code");
 	}
 	
-	public static void showCourseInfo(String inputName)
+		public static void showCourseInfo(String inputName)
 	{
-		System.out.println("This course is CPSC 1150");
+		System.out.println("\t****Welcome!!!");
+		
+		System.out.println("\nThis course is Introducing to Java Programming");
 		System.out.println("The instructor is: " + inputName + ".");
 		
 		System.out.println("Have fun!!!\n");
