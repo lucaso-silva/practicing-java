3b2646bfb4dd6971b3d231c9ff3180edbcfbb090
lucaso-silva <luc.osilva@gmail.com>
2023-09-29 22:05:03 -0700

    sum of number - for loop

---
+++ 4. while_do-while_for/SumOfNumbers.java
@@ -0,0 +1,13 @@
+public class SumOfNumbers
+{
+	public static void main(String[] args)
+	{
+		int sum = 0, num = 100;
+		
+		for(int i = 1; i <= 100; i++){
+			sum = sum + i;
+		}
+		
+		System.out.println("The sum of 1 + 2 + 3 + 4 + (...) up to 100 is: " + sum);
+	}
+}
